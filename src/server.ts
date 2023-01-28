f17a8cc9963ae53728315f177498d9fd14926636
dorianascimento <rafaelnd2001@gmail.com>
2023-01-28 09:16:30 -0300

    created conexion with bd, project is ready

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import mainRoutes from './routes/api';
 
 dotenv.config();
 
 const server = express();
 
+server.use(cors());
+
 server.use(express.urlencoded({extended: true}));
 
+server.use(mainRoutes);
+
 server.use((req: Request, res: Response)=>{
+    res.status(404);
     res.json({ error: 'endopoint não encontrado' });
 });
 
